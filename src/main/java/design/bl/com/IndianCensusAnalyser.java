ca8147daf99e601777d46467e3e12288446978e4
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-19 19:02:04 +0530

    first commit

---
+++ src/main/java/design/bl/com/IndianCensusAnalyser.java
@@ -0,0 +1,10 @@
+package design.bl.com;
+
+public class IndianCensusAnalyser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to Indian census analyser");
+	}
+
+}
